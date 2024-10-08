6b23462fbfcb4f2bd5fe4ff64d8327a147fa54dd
ggay <ggay@student.42.fr>
2024-10-08 17:20:06 +0200

    FIX: removed useless comments. TODO: check why bash starts in posix mode

--- srcs/realloc.c
+++ srcs/realloc.c
@@ -28,20 +28,14 @@ void *_realloc(void *ptr, size_t size) {
     if (size <= CHUNK_SIZE(chunk->size)) {
         return SKIP_HEADER_CHUNK(chunk);
     }
-    // return full_dealloc(chunk, size);
 
     size_t added_size = size - CHUNK_SIZE(chunk->size) + sizeof(t_chunk);
-	// ft_printf("added%d\n", added_size);
-	// ft_printf("asked %d\n", size);
-	// ft_printf("current %d\n", CHUNK_SIZE(chunk->size));
     if (CHUNK_FREE(chunk->next) && CHUNK_SIZE(chunk->next->size) > added_size) {
         t_chunk *extra_chunk = split_free_chunk(chunk->next, added_size);
         chunk->next = extra_chunk->next;
         extra_chunk->next->prev = chunk;
         chunk->size = (char *)chunk->next - (char *)chunk - sizeof(t_chunk);
         SET_CHUNK_USED(chunk);
-		// ft_printf("new chunk size %d\n", chunk->size);
-
         return SKIP_HEADER_CHUNK(chunk);
     } else {
         return full_dealloc(chunk, size);
